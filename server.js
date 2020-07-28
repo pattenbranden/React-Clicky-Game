2b80c40f7e60ccc13a96ede266a8e0c97002481e
Branden Patten <patten.branden@gmail.com>
2020-07-28 04:10:05 -0400

    changed some comments slightly

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// Our middleware:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+// using our routing config file
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-// Start the API server
+// Start listening on PORT
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
